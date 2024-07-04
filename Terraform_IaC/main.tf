fdc0f655177588873d7d5fcca2f85b7174526d29
box genius <115899706+box-genius@users.noreply.github.com>
2024-07-04 20:18:57 -0300

    complete

--- Terraform_IaC/main.tf
+++ Terraform_IaC/main.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "web" {
   count         = 10
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = aws_key_pair.deployer.key_name
 
   tags = {
     Name = "Minsait_${count.index}"
