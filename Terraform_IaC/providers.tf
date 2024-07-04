bb25a96117d1e7c348f15305c1fce8c8f72e284d
box genius <115899706+box-genius@users.noreply.github.com>
2024-07-04 20:13:42 -0300

    complete

--- Terraform_IaC/providers.tf
+++ Terraform_IaC/providers.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region = var.aws_region
+  access_key = var.aws_access_key != "" ? var.aws_access_key : env.AWS_ACCESS_KEY_ID
+  secret_key = var.aws_secret_key != "" ? var.aws_secret_key : env.AWS_SECRET_ACCESS_KEY
 }
 
 terraform {
