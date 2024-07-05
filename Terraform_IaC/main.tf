de4fc57f5ae526605ebb58dafb1886b285ffb0fa
box genius <115899706+box-genius@users.noreply.github.com>
2024-07-04 21:06:46 -0300

    complete

--- Terraform_IaC/main.tf
+++ Terraform_IaC/main.tf
@@ -15,9 +15,15 @@ resource "aws_instance" "web" {
               sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
               sudo apt-get update
               sudo apt-get install -y docker-ce
+              
+              if [ $? -eq 0 ]; then
+                echo "Docker installed successfully" > /var/log/docker-install.log
+              else
+                echo "Docker installation failed" > /var/log/docker-install.log
+              fi
+
               sudo usermod -aG docker ubuntu
               sudo systemctl enable docker
               sudo systemctl start docker
-              docker --version > /var/log/docker-version.log
               EOF
 }
